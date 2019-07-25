20b76f6c3052181d5fde8cda08668cc7a97a5475
Corey Forrieter <cforrieter@digitalmga.com>
2019-07-25 10:59:12 -0700

    remove unnecessary logging

--- index.js
+++ index.js
@@ -234,7 +234,6 @@ class ServerlessDynamodbLocal {
         if (!source.table) {
           throw new Error('seeding source "table" property not defined');
         }
-        this.serverlessLog("Sources: " + source.sources);
         const seedPromise = seeder
           .locateSeeds(source.sources || [])
           .then(seeds =>
@@ -245,7 +244,6 @@ class ServerlessDynamodbLocal {
             )
           );
 
-        this.serverlessLog("Source path: " + source.sourcePath);
         const seedFolderPromise = seeder
           .locateSeedsFiles(source.sourcePath)
           .then(seeds =>
